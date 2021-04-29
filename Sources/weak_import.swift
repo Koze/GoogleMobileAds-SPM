1bb06738df90fc19e723a2703109ff16172f6e78
Kazuma Koze <kazumakoze@gmail.com>
2021-04-29 09:49:43 +0900

    Update the reference of cocoapods spec

--- Sources/weak_import.swift
+++ Sources/weak_import.swift
@@ -5,7 +5,7 @@
 //  Created by Kazuma Koze on 2021/03/09.
 //
 
-// ref. https://github.com/CocoaPods/Specs/blob/6c4ab4f0d57f70816f3e2cfb8476c02b8c1a6e76/Specs/5/9/a/Google-Mobile-Ads-SDK/8.2.0/Google-Mobile-Ads-SDK.podspec.json#L56-L61
+// ref. https://github.com/CocoaPods/Specs/blob/2b30938515ab316ce0cfc3d1029dcd4e987ffb61/Specs/5/9/a/Google-Mobile-Ads-SDK/8.3.0/Google-Mobile-Ads-SDK.podspec.json#L54-L59
 
 #if canImport(AdSupport)
 import AdSupport
