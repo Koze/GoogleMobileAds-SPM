5aa3bd43d555e67f5a151e8d64c7f88a2ede4f24
Kazuma Koze <kazumakoze@gmail.com>
2021-11-20 00:13:26 +0900

    Update the reference of cocoapods spec

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
- // ref. https://github.com/CocoaPods/Specs/blob/0577d0d210fb3baee65eb0c2850f27662236a44b/Specs/5/9/a/Google-Mobile-Ads-SDK/8.12.0/Google-Mobile-Ads-SDK.podspec.json
+ // ref. https://github.com/CocoaPods/Specs/blob/e85a12ac6c4d5e43bc0ace037b14aa2e7b46098c/Specs/5/9/a/Google-Mobile-Ads-SDK/8.13.0/Google-Mobile-Ads-SDK.podspec.json
 
 import PackageDescription
 
