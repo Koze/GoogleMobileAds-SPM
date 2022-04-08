ffd25689f8986fd9639ae5c6f1d1f9406bb7e5b7
Kazuma Koze <kazumakoze@gmail.com>
2022-04-08 10:17:39 +0900

    Update the reference of cocoapods spec

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
- // ref. https://github.com/CocoaPods/Specs/blob/61d50feb1cdcfab1110ca471689b37d4a94341f1/Specs/5/9/a/Google-Mobile-Ads-SDK/9.2.0/Google-Mobile-Ads-SDK.podspec.json
+ // ref. https://github.com/CocoaPods/Specs/blob/61d50feb1cdcfab1110ca471689b37d4a94341f1/Specs/5/9/a/Google-Mobile-Ads-SDK/9.3.0/Google-Mobile-Ads-SDK.podspec.json
 
 import PackageDescription
 
--- Sources/weak_import.swift
+++ Sources/weak_import.swift
@@ -5,7 +5,7 @@
 //  Created by Kazuma Koze on 2021/03/09.
 //
 
-// ref. https://github.com/CocoaPods/Specs/blob/61d50feb1cdcfab1110ca471689b37d4a94341f1/Specs/5/9/a/Google-Mobile-Ads-SDK/9.2.0/Google-Mobile-Ads-SDK.podspec.json#L63-L68
+// ref. https://github.com/CocoaPods/Specs/blob/61d50feb1cdcfab1110ca471689b37d4a94341f1/Specs/5/9/a/Google-Mobile-Ads-SDK/9.3.0/Google-Mobile-Ads-SDK.podspec.json#L63-L68
 
 #if canImport(AdSupport)
 import AdSupport
